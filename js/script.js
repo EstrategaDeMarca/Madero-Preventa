711c39bb9291972249952027cfb85b90b79ae53b
Livier Alarcón <maderolivierrp@gmail.com>
2026-09-08 16:31:25 -0600

    Rename button event listener for floating cart

--- js/script.js
+++ js/script.js
@@ -529,7 +529,7 @@ function inicializarApp() {
     boton.addEventListener("click", () => irAPaso(boton.dataset.regresar, { validar: false }));
   });
 
-  document.getElementById("btn-ver-carrito-flotante").addEventListener("click", () => {
+  document.getElementById("btn-carrito-flotante").addEventListener("click", () => {
     irAPaso("carrito", { validar: false });
   });
 
